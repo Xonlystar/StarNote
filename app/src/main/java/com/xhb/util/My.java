52faa884d057c8c8cccde3e3fa0f8c9cf32eef72
onlystar <991923257@qq.com>
2016-08-29 14:02:31 +0800

    这是我的第一次提交

---
+++ app/src/main/java/com/xhb/util/My.java
@@ -0,0 +1,11 @@
+package com.xhb.util;
+
+import com.xhb.R;
+
+public class My		//设置皮肤颜色
+{
+	public static String[] cs={"激情红","温暖橙","明快黄","青春绿","自在青","宁静蓝","优雅紫","浪漫粉","寂寞灰","自定义"};
+	public static int[] colors={R.color.red,R.color.orange,R.color.yellow,
+			R.color.green,R.color.cyan,R.color.blue,R.color.purple,
+			R.color.pink,R.color.darkgray,R.color.white};
+}
